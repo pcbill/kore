df6d1ecc12585231644dfb976b263b6aafbf7274
Bill_SJ_Wu@wistron.com <Bill_SJ_Wu@wistron.com>
2017-12-27 11:14:22 +0800

    1. using Mockito Junit Runner instead of Spring Runner. 2, add jaxb for new jdk issue

--- src/test/kotlin/pcbill/starter/SampleApplicationTests.kt
+++ src/test/kotlin/pcbill/starter/SampleApplicationTests.kt
@@ -2,10 +2,10 @@ package pcbill.starter
 
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
+@RunWith(MockitoJUnitRunner::class)
 @SpringBootTest
 class SampleApplicationTests {
 
